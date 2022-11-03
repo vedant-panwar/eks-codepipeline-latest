377afa300d71f2685013863aa848c205d5b69841
vedant-panwar <84500225+vedant-panwar@users.noreply.github.com>
2022-11-03 22:45:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const {PORT = 8080} = process.env;
 
 console.log('PORT',PORT);
 
-app.get('/',(req,res)=> res.send(`Hello Bossssssss Node Server EKS-Ci/Cd`));
+app.get('/',(req,res)=> res.send(`Hello From Node Server By EKS-Ci/Cd`));
 
 
 app.listen(PORT,()=>{
